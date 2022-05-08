c99ee8644808d5e282ea54ac85d3f5905d833904
pingline <pingline@outlook.com>
2022-05-08 11:49:25 +0800

    feat(nvim): add config for session-manager

---
+++ nvim/lua/plugins/session-manager.lua
@@ -0,0 +1,15 @@
+return function()
+  require("session_manager").setup(
+    {
+    autoload_mode = require('session_manager.config').AutoloadMode.CurrentDir,
+  })
+
+  local config_group = vim.api.nvim_create_augroup('SessionAuGroup', {}) -- A global group for all your config autocommands
+
+  vim.api.nvim_create_autocmd({ 'SessionLoadPost' }, {
+    group = config_group,
+    callback = function()
+      require('nvim-tree').toggle(false, true)
+    end,
+  })
+end
