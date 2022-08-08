9172745f13328531733382447b261ad285607675
pingline <pingline@outlook.com>
2022-08-08 10:30:18 +0800

    feat(plugins.lsp): 禁用了Wolfram, 并且重构代码使LSPInstaller和Lsp Setup共享一次设置.

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -1,6 +1,7 @@
 return function()
+  local servers = { "clangd", "pyright", "bashls", "cmake", "texlab", "ltex", "powershell_es", "jsonls" }
   require('nvim-lsp-installer').setup({
-    ensure_installed = { "sumneko_lua", "bashls", "clangd", "cmake", "texlab", "pyright", "ltex" },
+    ensure_installed = servers,
     -- automatic_installation = true,
     ui = {
       icons = {
@@ -15,13 +16,14 @@ return function()
   local util = require("lspconfig.util")
   local configs = require("lspconfig.configs")
 
-  local servers = { "clangd", "pyright", "julials", "bashls", "cmake", "texlab", "ltex" }
 
   require("lsp_signature").setup()
 
   for _, lsp in ipairs(servers) do
     lspconfig[lsp].setup({})
   end
+
+  lspconfig['julials'].setup({})
 
   lspconfig.sumneko_lua.setup({
     settings = {
@@ -41,13 +43,13 @@ return function()
     }
   })
 
-  configs.lsp_wl = {
-    default_config = {
-      cmd = { "nc", "localhost", "6536" },
-      filetypes = { "mma", "wl" },
-      root_dir = util.path.dirname,
-    },
-  }
-
-  lspconfig.lsp_wl.setup({})
+  -- configs.lsp_wl = {
+  --   default_config = {
+  --     cmd = { "nc", "localhost", "6536" },
+  --     filetypes = { "mma", "wl" },
+  --     root_dir = util.path.dirname,
+  --   },
+  -- }
+  --
+  -- lspconfig['lsp-wl'].setup({})
 end
