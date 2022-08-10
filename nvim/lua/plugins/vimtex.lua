6924b5c3d6d789e9374ada4242f3d3aeaea2507f
pingline <pingline@outlook.com>
2022-08-10 20:08:29 +0800

    perf(snips): 更新了Markdown数学环境的文件引用.

--- nvim/lua/plugins/vimtex.lua
+++ nvim/lua/plugins/vimtex.lua
@@ -4,6 +4,7 @@ return function()
   vim.g.vimtex_view_general_options = "-reuse-instance -forward-search @tex @line @pdf"
   vim.g.vimtex_compiler_method = "latexmk"
   vim.g.vimtex_fold_enabled = true
+  vim.g.vim_markdown_math = 1
   -- 禁止Vimtex自动打开Quickfix
   vim.g.vimtex_quickfix_mode = 0
 end
