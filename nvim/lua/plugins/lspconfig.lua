e51c1fde2705e5afecba41e16f5d7ffac82ad743
pingline <pingline@outlook.com>
2022-08-31 08:15:32 +0800

    feat(nvim.lsp): Set keymap for lsp.

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -39,15 +39,41 @@ return function()
     },
   })
 
+  local on_attach = function(client, bufnr)
+    -- Enable completion triggered by <c-x><c-o>
+    vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+    -- Mappings.
+    -- See `:help vim.lsp.*` for documentation on any of the below functions
+    local bufopts = { noremap = true, silent = true, buffer = bufnr }
+    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
+    vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+  end
+
   require("lsp_signature").setup()
 
   for _, lsp in ipairs(servers) do
-    lspconfig[lsp].setup({})
+    lspconfig[lsp].setup({
+      on_attach = on_attach
+    })
   end
 
-  lspconfig['julials'].setup({})
+  for _, lsp in ipairs(manually_install_servers) do
+    lspconfig[lsp].setup({
+      on_attach = on_attach
+    })
+  end
+
+  lspconfig['julials'].setup({
+    on_attach = on_attach
+  })
 
   lspconfig.sumneko_lua.setup({
+    on_attach = on_attach,
     settings = {
       Lua = {
         diagnostics = {
@@ -66,6 +92,7 @@ return function()
   })
 
   lspconfig.efm.setup({
+    on_attach = on_attach,
     filetypes = { 'python' },
     root_dir = function() return vim.fn.getcwd() end;
     init_options = { documentFormatting = true },
