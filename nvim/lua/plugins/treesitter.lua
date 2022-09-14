e1dce03c50cb767042df2657e89e0535bc7b04a1
yqwu905 <pingline@outlook.com>
2022-09-14 21:01:54 +0800

    Remove phpdoc due to install issue.

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -2,7 +2,7 @@ return function()
   require 'nvim-treesitter.configs'.setup {
     ensure_installed = "all",
     sync_install = true,
-    ignore_install = { "latex", "markdown" },
+    ignore_install = { "latex", "markdown", "phpdoc" },
 
     highlight = {
       enable = true,
