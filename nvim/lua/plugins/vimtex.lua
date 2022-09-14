383d012665ae13939447e4b324fc34b4a8e02f99
yqwu905 <pingline@outlook.com>
2022-09-14 21:02:13 +0800

    Set preview and reverse search for zathura.

--- nvim/lua/plugins/vimtex.lua
+++ nvim/lua/plugins/vimtex.lua
@@ -1,7 +1,7 @@
 return function()
   vim.g.tex_flavor = "latex"
-  vim.g.vimtex_view_general_viewer = "SumatraPDF.exe"
-  vim.g.vimtex_view_general_options = "-reuse-instance -forward-search @tex @line @pdf"
+  vim.g.vimtex_view_method = 'zathura'
+  vim.g.vimtex_compiler_progname = "nvr"
   vim.g.vimtex_compiler_method = "latexmk"
   vim.g.vimtex_fold_enabled = true
   vim.g.vim_markdown_math = 1
