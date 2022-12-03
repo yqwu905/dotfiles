2b47cd4e66556c6f0e0db7cf9abe93c1ada9bdfc
yqwu905 <pingline@outlook.com>
2022-12-03 10:21:03 +0800

    polybar: 修复了当前播放脚本-在无法获取信息时不会显示错误信息.

--- polybar/scripts/music.sh
+++ polybar/scripts/music.sh
@@ -2,14 +2,20 @@
 
 player_status=$(playerctl status 2> /dev/null)
 if [[ $? -eq 0 ]]; then
-    metadata="$(playerctl metadata artist) - $(playerctl metadata title)"
+  player=$(playerctl metadata artist 2> /dev/null)
+  title=$(playerctl metadata title 2> /dev/null)
+  if [ -z "$player" ]; then
+    metadata="$title"
+  else
+    metadata="$player - $title"
+  fi
 fi
 
 # Foreground color formatting tags are optional
 if [[ $player_status = "Playing" ]]; then
-    echo "%{F#FFFFFF}$metadata%{F-}"
+  echo "%{F#FFFFFF}$metadata%{F-}"
 elif [[ $player_status = "Paused" ]]; then
-    echo "%{F#999}$metadata%{F-}"
+  echo "%{F#999}$metadata%{F-}"
 else
-    echo ""
+  echo ""
 fi
