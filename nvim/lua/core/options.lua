16d0784c2d83b876653fbd741a8d618aab549571
yqwu905 <pingline@outlook.com>
2022-12-09 07:41:01 +0800

    nvim: Use opt field rather than vim.cmd

--- nvim/lua/core/options.lua
+++ nvim/lua/core/options.lua
@@ -75,5 +75,5 @@ opt.textwidth = 100
 opt.colorcolumn = '80'
 opt.conceallevel = 2
 -- opt.concealcursor = 'niv'
+opt.relativenumber = true
 vim.g.python3_host_prog = '/usr/bin/python'
-vim.cmd("set relativenumber")
