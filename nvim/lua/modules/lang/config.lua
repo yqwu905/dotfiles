e4902c716027283c44b1f82afc48ba11d5df8435
yqwu905 <pingline@outlook.com>
2022-12-10 17:51:03 +0800

    nvim: format

--- nvim/lua/modules/lang/config.lua
+++ nvim/lua/modules/lang/config.lua
@@ -1,10 +1,10 @@
 local config = {}
 
 function config.vimtex()
-  vim.g.tex_flavor = "latex"
+  vim.g.tex_flavor = 'latex'
   vim.g.vimtex_view_method = 'zathura'
-  vim.g.vimtex_compiler_progname = "nvr"
-  vim.g.vimtex_compiler_method = "latexmk"
+  vim.g.vimtex_compiler_progname = 'nvr'
+  vim.g.vimtex_compiler_method = 'latexmk'
   vim.g.vimtex_fold_enabled = true
   vim.g.vim_markdown_math = 1
   -- 禁止Vimtex自动打开Quickfix
